369a14b950118d507ff29375deb7d61b07583e6d
Ayush Dhiman <ayush@ezaix.com>
2024-11-11 22:26:57 +0530

    added: logging in backend

--- src/rest/rest/views.py
+++ src/rest/rest/views.py
@@ -5,6 +5,8 @@ from rest_framework import status
 import json, logging, os
 from pymongo import MongoClient
 
+logger = logging.getLogger(__name__)
+
 mongo_uri = 'mongodb://' + os.environ["MONGO_HOST"] + ':' + os.environ["MONGO_PORT"]
 db = MongoClient(mongo_uri)['test_db']
 todo_collection = db['todos']
@@ -13,8 +15,12 @@ class TodoListView(APIView):
 
     def get(self, request):
         # Implement this method - return all todo items from db instance above.
-        todos = list(todo_collection.find({}, {"_id":0}))
-        return Response(todos, status=status.HTTP_200_OK)
+        try:
+            todos = list(todo_collection.find({}, {"_id":0}))
+            return Response(todos, status=status.HTTP_200_OK)
+        except Exception as e:
+            logger.error(f"Error in fetching todos: {str(e)}")
+            return Response({"error":str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
     def post(self, request):
         # Implement this method - accept a todo item in a mongo collection, persist it using db instance above.
@@ -27,6 +33,7 @@ class TodoListView(APIView):
             todo_collection.insert_one({"todo":data})
             return Response({"message": data+" Added Successfully"}, status=status.HTTP_201_CREATED)
         except Exception as e:
+            logger.error(f"Error in adding todos: {str(e)}")
             return Response({"error":str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
             
 
